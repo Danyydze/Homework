81a8b6f1f300881d9d51f3120d88db76c94afab4
Danyydze <danilamarkov2803@icloud.com>
2025-01-30 19:26:56 +0300

    3th Sex

--- Lesson4-25/ViewController.swift
+++ Lesson4-25/ViewController.swift
@@ -8,22 +8,80 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Basic UI elements
+    
+    private let leadingInset: CGFloat = 16
+    
+    private lazy var mainInfoLabel: (String, CGFloat) -> UILabel = { content, size in
+        let label = UILabel()
+        label.font = .systemFont(ofSize: size, weight: .medium)
+        label.text = content
+        return label
+    }
+    
+    private lazy var subInfoLabel: (String) -> UILabel = {content in
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .gray
+        return label
+        
+    }
+    
+    private lazy var profilePie: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "person.circle.fill"))
+        
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 25
+        imageView.clipsToBounds = true
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: 50),
+            imageView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        return imageView
+    }()
 
     //MARK: User info entries
     
     private lazy var sex = UserInfoEntery(heading: "SEX", info:"Dima")
     
+    //MARK: Definition of headStackView
+    
+    private lazy var headStackView: UIStackView = {
+        let horizontalStack = UIStackView()
+        horizontalStack.axis = .horizontal
+        
+        let verticalStack = UIStackView()
+        verticalStack.axis = .vertical
+        
+        verticalStack.addArrangedSubview(self.mainInfoLabel("John Dob", 17))
+        verticalStack.addArrangedSubview(self.subInfoLabel("01.01.1970"))
+        horizontalStack.addArrangedSubview(verticalStack)
+        horizontalStack.addArrangedSubview(profilePie)
+        
+        return horizontalStack
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(sex)
-        
-        sex.translatesAutoresizingMaskIntoConstraints = false
-        sex.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        sex.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        view.backgroundColor = .systemBackground
+        setupHeadStack()
     }
     
 
-
+    private func setupHeadStack() {
+        view.addSubview(headStackView)
+        headStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            headStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leadingInset)
+        ])
+    }
 
 }
 
